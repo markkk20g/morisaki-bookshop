f28556b9873dc790fc829690bbe1a911d785bf2c
markkk20g <abdulmalik.1122@mail.ru>
2026-05-27 21:39:01 +0900

    fix: modify HomeNavbar components

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Box, Button, Container, Stack } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useHistory } from "react-router-dom";
 import Topbar from "./Topbar";
 import { useGlobals } from "../../hooks/useGlobals";
 import { CartItem } from "../../../libs/types/search";
@@ -37,74 +35,10 @@ export default function HomeNavbar(props: HomeNavbarProps) {
   } = props;
 
   const {authMember} = useGlobals();
+  const history = useHistory();
 
   return (
     <div className="home-navbar">
-      {/* <div className="navbar-menu-frame">
-        <Container className="navbar-container">
-          <Stack className="menu-items">
-            <Box sx={{ bgcolor: "#ff8b8b" }}>
-              <NavLink to="/">
-                <img className="brand-logo" src="/icons/white1.png" />
-              </NavLink>
-            </Box>
-
-            <Stack className="menu-links">
-              <Box className="hover-box">
-                <NavLink to="/" activeClassName="underline">
-                  HOME
-                </NavLink>
-              </Box>
-              <Box className="hover-box">
-                <NavLink to="/" activeClassName="underline">
-                  BOOKS
-                </NavLink>
-              </Box>
-              <Box className="hover-box">
-                <NavLink to="/" activeClassName="underline">
-                  EVENTS
-                </NavLink>
-              </Box>
-              {authMember ? (
-                <Box className="hover-box">
-                  <NavLink to="/" activeClassName="underline">
-                    ORDERS
-                  </NavLink>
-                </Box>
-              ) : null}
-              {authMember ? (
-                <Box className="hover-box">
-                  <NavLink to="/" activeClassName="underline">
-                    MY PAGE
-                  </NavLink>
-                </Box>
-              ) : null}
-              <Box className="hover-box">
-                <NavLink to="/" activeClassName="underline">
-                  HELP
-                </NavLink>
-              </Box>
-            </Stack>
-
-            <Box className="menu-search">
-              <input type="text" placeholder="Seach..." className="input-area" />
-              <Box>
-                <Button
-                  variant="contained"
-                  sx={{ height: "50px", color: "#d1d5db" }}
-                >
-                  I
-                </Button>
-              </Box>
-            </Box>
-
-            <Box className="menu-shorts">
-              <AccountBoxIcon />
-              <ShoppingCartIcon />
-            </Box>
-          </Stack>
-        </Container>
-      </div> */}
       <Topbar 
         cartItems={cartItems}
         onAdd={onAdd}
@@ -123,7 +57,6 @@ export default function HomeNavbar(props: HomeNavbarProps) {
       <div className="header-frame">
         <div className="header-frame-gradient" />
         <Container className="header-frame-container">
-          {/* <div className="header-frame-gradient" /> */}
           <Stack className="header-detail">
             <Box className="header-heading">
               <h1>Discover your next read</h1>
@@ -133,9 +66,10 @@ export default function HomeNavbar(props: HomeNavbarProps) {
                 curated selection of contemporary masterpieces and timeless classics.</p>
             </Box>
             <Stack className="header-btns">
-              {/* <a className="header-explore-btn">EXPLORE BOOKS</a> */}
-              {/* <a className="header-signup-btn">SIGN UP</a> */}
-              <Button className="header-explore-btn">
+              <Button 
+                className="header-explore-btn"
+                onClick={() => history.push('/products')}
+              >
                 EXPLORE BOOKS
               </Button>
               {!authMember ? (
@@ -147,26 +81,6 @@ export default function HomeNavbar(props: HomeNavbarProps) {
                 </Button>
               ) : (null)}
             </Stack>
-
-            {/* <Stack className="header-info">
-              <Box className="slogan">
-                <img className="slogan-img" src="/img/slogan.png" />
-              </Box>
-              <Box className="lead-in">Find Your Next Great Read Today</Box>
-              <Box className="shop-button-box">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className="shop-button"
-                >
-                  SHOP NOW
-                </Button>
-              </Box>
-            </Stack> */}
-
-            {/* <Box className="header-img">
-              <img className="img-item" src="/img/navbar-img.png" />
-            </Box> */}
           </Stack>
         </Container>
       </div>
